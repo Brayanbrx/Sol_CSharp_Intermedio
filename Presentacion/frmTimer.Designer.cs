9290b519b8d882956f2c15a503b5870b73cf4d51
Brayanbrx <brayanbrx1@gmail.com>
2023-09-24 23:45:47 -0400

    mas cambios

--- Presentacion/frmTimer.Designer.cs
+++ Presentacion/frmTimer.Designer.cs
@@ -29,11 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbContador = new System.Windows.Forms.Label();
+            this.txtContador = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lbContador
+            // 
+            this.lbContador.AutoSize = true;
+            this.lbContador.Location = new System.Drawing.Point(102, 82);
+            this.lbContador.Name = "lbContador";
+            this.lbContador.Size = new System.Drawing.Size(50, 13);
+            this.lbContador.TabIndex = 0;
+            this.lbContador.Text = "Contador";
+            // 
+            // txtContador
+            // 
+            this.txtContador.Location = new System.Drawing.Point(172, 82);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.ReadOnly = true;
+            this.txtContador.Size = new System.Drawing.Size(100, 20);
+            this.txtContador.TabIndex = 1;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(102, 134);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(104, 23);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Iniciar o Continuar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(212, 134);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(104, 23);
+            this.btnDetener.TabIndex = 3;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(957, 37);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // frmTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1032, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtContador);
+            this.Controls.Add(this.lbContador);
+            this.Name = "frmTimer";
             this.Text = "frmTimer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbContador;
+        private System.Windows.Forms.TextBox txtContador;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- Presentacion/frmTimer.cs
+++ Presentacion/frmTimer.cs
@@ -12,9 +12,53 @@ namespace Presentacion
 {
     public partial class frmTimer : Form
     {
+
+        //Declaracion de variables para el formulario
+        int Contador = 0; //inicializando variable para timer
+        //fin declaracion de variables
         public frmTimer()
         {
             InitializeComponent();
+        }
+
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*if (Contador < 100)
+            {
+                Contador++;
+                txtContador.Text = Convert.ToString(Contador);
+                //progressBar1.Value = Contador;
+                progressBar1.Increment(1);
+            }
+            else
+            {
+                timer1.Stop();
+            }*/
+            //Segunda forma de trabajo con progressbar y timer
+            if(progressBar1.Value < 100)
+            {
+                progressBar1.Increment(1);
+                txtContador.Text = Convert.ToString(progressBar1.Value);
+            }
+            else
+            {
+                timer1.Stop();
+            }
+                
+
+            
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); 
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            timer1 .Stop();
         }
     }
 }
