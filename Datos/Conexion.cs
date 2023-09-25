4484ae23e019840021f5472fa4e399233e66e1e6
Brayanbrx <brayanbrx1@gmail.com>
2023-09-25 17:39:02 -0400

    sql conexion

---
+++ Datos/Conexion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+
+        private Conexion()
+        {
+            this.Base = "MiBaseDatos";
+            this.Servidor = "DESKTOP-HP44CUK\\SQLEXPRESS";
+            this.Usuario = "brayanbrx1";
+            this.Clave = "Elohell245";
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database =" + this.Base + "; User Id =" + this.Usuario + "; Password=" + this.Clave; 
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+        public static Conexion GetInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+
+
+    }
+}
